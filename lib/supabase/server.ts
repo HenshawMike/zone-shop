4a8db8708b1df0b1237d9f7692752b3cfbddb22a
HenshawMike <mikelhenshaw11@gmail.com>
2026-03-12 01:34:16 +0100

    done for now

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -3,8 +3,10 @@ import { cookies } from "next/headers"
 
 export async function createClient() {
   const cookieStore = await cookies()
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+  return createServerClient(supabaseUrl, supabaseAnonKey, {
     cookies: {
       get(name: string) {
         return cookieStore.get(name)?.value
@@ -13,16 +15,14 @@ export async function createClient() {
         try {
           cookieStore.set({ name, value, ...options })
         } catch (error) {
-          // The `set` method was called from a Server Component.
-          // This can be ignored if you have middleware refreshing
-          // user sessions.
+          // Handled
         }
       },
       remove(name: string, options: CookieOptions) {
         try {
           cookieStore.set({ name, value: "", ...options })
         } catch (error) {
-          // The `delete` method was called from a Server Component.
+          // Handled
         }
       },
     },
--- lib/supabase/static.ts
+++ lib/supabase/static.ts
@@ -2,8 +2,7 @@ import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
 // Create a Supabase client for server-side data fetching during build time.
 export const createClient = () => {
-  return createSupabaseClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  return createSupabaseClient(supabaseUrl, supabaseAnonKey);
 };
